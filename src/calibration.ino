76385b6072aae6f967b4a38ca6950217968a9d5f
fy0d_0r <134260747+fy0d-0r@users.noreply.github.com>
2024-08-01 02:07:12 +0630

    Create calibration.ino

---
+++ src/calibration.ino
@@ -0,0 +1,24 @@
+#include "HX711.h"
+
+HX711 scale;
+
+void setup() {
+  // Initializing the Serial Communication
+  Serial.begin(9600);
+  Serial.println("Initializing the scale");
+  
+  // Initializing the HX711 scale
+  scale.begin(4, 5); //DT, SCK
+  scale.set_scale();
+  scale.tare();
+}
+
+void loop() {
+  float data = scale.get_units(10);
+  Serial.print(data);
+  Serial.println(" units");
+
+  delay(1000);
+}
+
+//420.89795918367346938775510204082
